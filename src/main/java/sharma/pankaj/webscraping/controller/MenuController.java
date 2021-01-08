9406b98cf3eb220bebc1704d1ce5a6adc810d25f
pankaj <pankaj_046@yahoo.in>
2021-01-08 23:24:09 +0530

    add menu api

--- src/main/java/sharma/pankaj/webscraping/controller/MenuController.java
+++ src/main/java/sharma/pankaj/webscraping/controller/MenuController.java
@@ -5,10 +5,15 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sharma.pankaj.webscraping.model.MenuModel;
+import sharma.pankaj.webscraping.model.PageItemModel;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -18,18 +23,45 @@ public class MenuController {
 
 
     @GetMapping("/")
-    public String getMenu() throws IOException {
-        Document doc = Jsoup.connect("https://www.allitebooks.in/").get();
-        Elements newsHeadlines = doc.select("div.td-ss-main-content");
-        System.out.println("sdsdsds : "+newsHeadlines);
-        String value = "sdsds";
-//        for (Element headline : newsHeadlines) {
-//            value = "\n" +headline;
-////            value = headline.attr("title"), headline.absUrl("href");
-//
-//
-//        }
+    public ResponseEntity<MenuModel> getMenu(){
+        MenuModel response = new MenuModel();
+        try {
+            List<MenuModel.Data> data = new ArrayList<>();
+            Document doc = Jsoup.connect("https://www.allitebooks.in/").get();
+            Elements body = doc.select("ul.sf-menu > li");
+            for (int i=0; i<6; i++){
+                Element element = body.get(i);
+                if (i==0){
+                    data.add(new MenuModel.Data(element.text(), "/", null));
+                }else if (i == 1 || i==5){
+                    String url1 = element.select("li > a").attr("href");
+                    String[] id = url1.split("/");
+                    List<MenuModel.Data.tag> tags = new ArrayList<>();
+                    Elements elementsTag = element.select("ul.sub-menu > li");
+                    for (Element element1 : elementsTag){
+                        String url = element1.select("li > a").attr("href");
+                        String[] a = url.split("/");
+                        tags.add(new MenuModel.Data.tag(element1.text(), a[a.length - 1]));
+                    }
+                    String title = element.text();
+                    String[] t1 = title.split(" ");
+                    if (i == 1){
+                        data.add(new MenuModel.Data(t1[0], id[id.length - 1], tags));
+                    }else {
+                        data.add(new MenuModel.Data(t1[0] + " "+t1[1], id[id.length - 1], tags));
+                    }
 
-        return newsHeadlines.toString();
+                }else {
+                    String url = element.select("li > a").attr("href");
+                    String[] a = url.split("/");
+                    data.add(new MenuModel.Data(element.text(), a[a.length - 1], null));
+                }
+            }
+            response = new MenuModel(false, "Data Fetch Successfully", data);
+        }catch (Exception e){
+            response = new MenuModel(true, "Data Not Found", null);
+        }
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/sharma/pankaj/webscraping/model/MenuModel.java
+++ src/main/java/sharma/pankaj/webscraping/model/MenuModel.java
@@ -5,25 +5,108 @@ import java.util.List;
 
 public class MenuModel {
 
-//    private boolean error;
-//    private String
-//
-//    private String name;
-//    private List<String> subMenuList;
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public List<String> getSubMenuList() {
-//        return subMenuList;
-//    }
-//
-//    public void setSubMenuList(List<String> subMenuList) {
-//        this.subMenuList = subMenuList;
-//    }
+    private boolean error;
+    private String message;
+    private List<Data> data;
+
+    public MenuModel() {
+    }
+
+    public MenuModel(boolean error, String message, List<Data> data) {
+        this.error = error;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Data> getData() {
+        return data;
+    }
+
+    public void setData(List<Data> data) {
+        this.data = data;
+    }
+
+    public static class Data{
+
+        String title;
+        String id;
+        List<tag> tag;
+
+        public Data() {
+        }
+
+        public Data(String title, String id, List<Data.tag> tag) {
+            this.title = title;
+            this.id = id;
+            this.tag = tag;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public List<Data.tag> getTag() {
+            return tag;
+        }
+
+        public void setTag(List<Data.tag> tag) {
+            this.tag = tag;
+        }
+
+        public static class tag{
+            String subTitle;
+            String id;
+
+            public tag(String subTitle, String id) {
+                this.subTitle = subTitle;
+                this.id = id;
+            }
+
+            public String getSubTitle() {
+                return subTitle;
+            }
+
+            public void setSubTitle(String subTitle) {
+                this.subTitle = subTitle;
+            }
+
+            public String getId() {
+                return id;
+            }
+
+            public void setId(String id) {
+                this.id = id;
+            }
+        }
+
+    }
+
 }
